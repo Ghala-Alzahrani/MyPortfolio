06ce72ced89825ca4444380b8975442ed7fed033
Ghala <galaa20300@gmail.com>
2026-06-20 15:17:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -12,3 +12,11 @@ const nav = document.querySelector('.nav-links');
 toggle.addEventListener('click', () => {
   nav.classList.toggle('active');
 });
+
+const links = document.querySelectorAll('.nav-links a');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    nav.classList.remove('active');
+  });
+});
